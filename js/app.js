00bc1b896ac16580bf9e6ecb0189073e2a4b10cb
MalickBr <malickfowoue@gmail.com>
2026-03-12 12:36:08 +0100

    section  resume et footer

--- js/app.js
+++ js/app.js
@@ -89,3 +89,24 @@ document.addEventListener('DOMContentLoaded', () => {
         activeProjets();
     });
 });
+
+
+const resumeBtns = document.querySelectorAll('.resume-btn');
+
+resumeBtns.forEach((btn, idx) => {
+    btn.addEventListener('click', () =>{
+        const resumeDetails = document.querySelectorAll('.resume-detail');
+
+        resumeBtns.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        btn.classList.add('active');
+
+        resumeDetails.forEach(detail => {
+            detail.classList.remove('active');
+        });
+        resumeDetails[idx].classList.add('active');
+    });
+});
+
+
